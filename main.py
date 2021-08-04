429e943b930d53632e3a478ea9a86b64640fdecc
zzp-seeker <957715602@qq.com>
2021-08-04 12:27:19 +0800

    zp-v3

--- main.py
+++ main.py
@@ -59,19 +59,19 @@ def ncov_report(username, password, name, is_useold):
     )
     if report_res.status_code != 200:
         raise RuntimeError('report_res 状态码不是 200')
-    return report_res.text
+    return post_data,report_res.text
 
 successs,ress,usernames,names = [],[],[],[]
 for user in  users:
     success=True
     username,password,name,useold=user
     try:
-        res = ncov_report(username=username,password=password,name=name,is_useold=(useold==0))
+        data,res = ncov_report(username=username,password=password,name=name,is_useold=(useold==0))
     except:
         success = False
         res = traceback.format_exc()
 
-    print(f'成功：服务器的返回是：\n\n{res}' if success else f'失败：发生如下异常：\n\n{res}')
+    print(f'{name}填报成功!\n填报数据:\n{data}\n服务器返回数据:\n{res}' if success else f'{name}填报失败:\n填报数据:\n{data}\n发生如下异常:\n{res}')
 
     successs+=[success]
     ress+=[res]
